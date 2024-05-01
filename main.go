b6c058473a5c64e4852f117535671267ad0b6580
Éderson Cássio <edersoncassio1983@gmail.com>
2024-05-01 12:18:54 -0300

    go-redsync configurado

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-redsync/redsync/v4/redis/goredis/v9"
+	goredislib "github.com/redis/go-redis/v9"
 )
 
 func main() {
+	client := goredislib.NewClient(&goredislib.Options{
+		Addr: "localhost:6379",
+	})
+
+	pool := goredis.NewPool(client)
+
 	router := gin.Default()
 
 	router.GET("/ping", func(c *gin.Context) {
